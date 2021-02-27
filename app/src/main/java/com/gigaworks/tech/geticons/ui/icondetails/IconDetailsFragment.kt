87968bda625fe040e3c1056cf3306ac14d1154a1
Archit Raj <arch10@ymail.com>
2021-02-27 17:37:05 +0530

    feat: add icon download feature

--- app/src/main/java/com/gigaworks/tech/geticons/ui/icondetails/IconDetailsFragment.kt
+++ app/src/main/java/com/gigaworks/tech/geticons/ui/icondetails/IconDetailsFragment.kt
@@ -1,11 +1,15 @@
 package com.gigaworks.tech.geticons.ui.icondetails
 
+import android.app.DownloadManager
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
@@ -17,6 +21,9 @@ import com.gigaworks.tech.geticons.databinding.FragmentIconDetailsBinding
 import com.gigaworks.tech.geticons.domain.Author
 import com.gigaworks.tech.geticons.ui.base.BaseFragment
 import com.gigaworks.tech.geticons.util.visible
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+
 
 class IconDetailsFragment : BaseFragment<FragmentIconDetailsBinding>() {
 
@@ -41,7 +48,7 @@ class IconDetailsFragment : BaseFragment<FragmentIconDetailsBinding>() {
             readme.text = icon.readme
             license.text = icon.license
 
-            if(icon.authorWebsite.isNullOrEmpty()) {
+            if (icon.authorWebsite.isNullOrEmpty()) {
                 website.visible(false)
             } else {
                 website.setOnClickListener {
@@ -51,22 +58,22 @@ class IconDetailsFragment : BaseFragment<FragmentIconDetailsBinding>() {
                 }
             }
 
-            if(icon.readme.isNullOrEmpty()) {
+            if (icon.readme.isNullOrEmpty()) {
                 readme.text = "N/A"
             }
 
             author.setOnClickListener {
                 val author = Author(
                     name = icon.authorName!!,
-                    website = icon.authorWebsite?:"",
+                    website = icon.authorWebsite ?: "",
                     id = icon.authorId!!,
-                    username = icon.authorUsername?:""
+                    username = icon.authorUsername ?: ""
                 )
                 val action = IconDetailsFragmentDirections.iconDetailsShowAuthor(author)
                 findNavController().navigate(action)
             }
 
-            if(icon.imgUrl.isNotEmpty()) {
+            if (icon.imgUrl.isNotEmpty()) {
                 val glideUrl = GlideUrl(
                     icon.imgUrl,
                     LazyHeaders.Builder()
@@ -78,6 +85,37 @@ class IconDetailsFragment : BaseFragment<FragmentIconDetailsBinding>() {
                     .placeholder(R.mipmap.ic_launcher_round)
                     .into(binding.iconImg)
             }
+
+            download.setOnClickListener {
+                if(icon.imgUrl.isNotEmpty()) {
+                    downloadImageNew(icon.imgUrl)
+                } else {
+                    Snackbar.make(binding.root, "Invalid download URL", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun downloadImageNew(downloadUrlOfImage: String) {
+        try {
+            val filename = System.currentTimeMillis().toString()
+            val dm = (activity?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager)
+            val downloadUri = Uri.parse(downloadUrlOfImage)
+            val request = DownloadManager.Request(downloadUri)
+                .addRequestHeader("Authorization", "Bearer $AUTH_TOKEN")
+            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+                .setAllowedOverRoaming(false)
+                .setTitle(filename)
+                .setMimeType("image/jpeg")
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationInExternalPublicDir(
+                    Environment.DIRECTORY_PICTURES,
+                    File.separator.toString() + filename + ".jpg"
+                )
+            dm.enqueue(request)
+            Toast.makeText(context, "Image download started.", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            Toast.makeText(context, "Image download failed.", Toast.LENGTH_SHORT).show()
         }
     }
 
